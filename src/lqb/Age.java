e99ab67d54b44fe88968631ef0fd46ac63fec1fd
twilightsnow9 <3278546510@qq.com>
2024-03-29 08:57:48 +0800

    2024/3/29

--- src/lqb/Age.java
+++ src/lqb/Age.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class Age {
     public static void main(String[] args) {
-//        for (int a = 1; a < 100; a++) {
-//            for (int b = 1; b < 200; b++) {
-//                    if (b / a == 2 && (double) (b + 20) /(a + 20) == 1.5 && b % a == 0) {
-//                        System.out.println(a + 20);
-//                    }
-//                }
-//        }
-//        Scanner sc = new Scanner(System.in);
-//        List list = new ArrayList<>();
-//        Iterator iterator = list.iterator();
+        for (int a = 1; a < 100; a++) {
+            for (int b = 1; b < 200; b++) {
+                    if (b / a == 2 && (double) (b + 20) /(a + 20) == 1.5 && b % a == 0) {
+                        System.out.println(a + 20);
+                    }
+                }
+        }
+        Scanner sc = new Scanner(System.in);
+        List list = new ArrayList<>();
+        Iterator iterator = list.iterator();
 
 //        网友年龄
-
+//
 //        父亲的年龄
 //        for (int i = 1; i < 100; i++) {
 ////            我的儿子年龄
